dc2600f81a42931e95443a38b19d03bcc718f772
AnjaneyuluReddy64EMMVEE <veeranjaneyulu@emmvee.in>
2025-07-31 15:58:51 +0530

    feat: Update font size in SplashScreen for improved readability

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
-    fontSize: heightPercentageToDP('5.1%'),
+    fontSize: heightPercentageToDP('5%'),
     fontWeight: 'bold',
   },
 });
